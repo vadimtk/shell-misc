21a4e5c86715505bdb7e5e9b1f59a4c8dbbcdb09
Vadim Tkachenko <vadim@percona.com>
2015-10-08 14:26:29 -0700

    Update clone-and-build-ps-toku.sh

--- clone-and-build-ps-toku.sh
+++ clone-and-build-ps-toku.sh
@@ -6,5 +6,12 @@ ln -s ../../../ft-index tokudb-engine/storage/tokudb
 mkdir percona-server-5.6-build
 mkdir percona-server-5.6-install
 cd percona-server-5.6-build
+
+#git clone --recursive git@github.com:percona/percona-server -b 5.6
+#cd percona-server/storage/Tokudb/PerconaFT
+#git remote add test <other_repo>
+#git fetch <other_repo>
+#git checkout <branch>
+
 #cmake ..
 #make install
